6ef09c095b8fe328218f0381ab54590d492ec5c5
datCalata <jorge.cmaeso@alumnos.upm.es>
2017-01-26 00:07:39 +0100

    Add files via upload
    Initial branch

---
+++ CancionAlana/CancionAlana.ino
@@ -0,0 +1,64 @@
+/*
+ Name:		CancionAlana.ino
+ Created:	25/01/2017 21:26:59
+ Author:	jcala
+*/
+#include "Pitches.h" 
+// the setup function runs once when you press reset or power the board
+int melody[] = {
+	NOTE_G4, NOTE_G4, NOTE_C5, NOTE_G4, NOTE_E4,
+	NOTE_E4, NOTE_G4, NOTE_A5, NOTE_AS5, NOTE_C5, NOTE_E5,
+	NOTE_F5, NOTE_C5, NOTE_A5, NOTE_C5, NOTE_F4,
+	NOTE_FS4, NOTE_A5, NOTE_D5, NOTE_FS5, NOTE_A6,NOTE_FS5,
+	NOTE_G5, NOTE_D5, NOTE_B5,NOTE_D5,NOTE_F5,
+	NOTE_E5, NOTE_C5, NOTE_G4, NOTE_C5, NOTE_D5, NOTE_E5,
+	NOTE_F5, NOTE_CS5, NOTE_GS5, NOTE_CS5, NOTE_F5,
+	NOTE_FS5, NOTE_C5, NOTE_A5, NOTE_D5, NOTE_FS5,
+	NOTE_GS5,NOTE_E5,NOTE_CS5,NOTE_GS5,
+	NOTE_A6,NOTE_G5,NOTE_F5,NOTE_E5,NOTE_D5,NOTE_C5,
+	NOTE_B5, NOTE_A5, NOTE_B5, NOTE_D5,
+	NOTE_E5,NOTE_D5,NOTE_C5,NOTE_B5,NOTE_A5,NOTE_B5,
+	NOTE_C5,NOTE_B5,NOTE_A5,NOTE_G4,NOTE_F4,NOTE_G4,
+	NOTE_A5,NOTE_G4,NOTE_F4,NOTE_E4,NOTE_F4,NOTE_A5,
+	NOTE_GS4,NOTE_E4,NOTE_F4,NOTE_E4,NOTE_GS4,NOTE_E4,
+	NOTE_A5, 0
+	
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+	4, 8, 8, 8, 8,
+	8, 8, 8, 8, 8, 8,
+	4, 8, 8, 8, 8,
+	8, 8, 8, 8, 8, 8,
+	4, 8, 8, 8, 8,
+	8, 8, 8, 8, 8, 8,
+	4, 8, 8, 8, 8,
+	4, 8, 8, 8, 8,
+	4, 8, 8, 8, 8,
+	8, 8, 8, 8, 8, 8,
+	8, 8, 3, 8,
+	8, 8, 8, 8, 8, 8,
+	8, 8, 8, 8, 8, 8,
+	8, 8, 8, 8, 8, 8,
+	8, 8, 8, 8, 8, 8,
+	8, 2
+};
+int tamano = 84;
+int pinBuzzer = 3;
+
+void setup() {
+
+}
+
+void loop() {
+	for (int thisNote = 0; thisNote < tamano; thisNote++) {
+
+		int noteDuration = 1000 / noteDurations[thisNote];
+		tone(pinBuzzer, melody[thisNote], noteDuration);
+		int pauseBetweenNotes = noteDuration * 1.30;
+		delay(pauseBetweenNotes);
+		noTone(pinBuzzer);
+	}
+	delay(3000);
+}
---
+++ CancionAlana/Debug/CancionAlana.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\jcala\AppData\Local\Temp\VMBuilds\CancionAlana\uno\Debug\CancionAlana.cpp.o: \
+ C:\Users\jcala\AppData\Local\Temp\VMBuilds\CancionAlana\uno\Debug\CancionAlana.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\jcala\AppData\Local\Temp\VMBuilds\CancionAlana\uno\Debug\Pitches.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\0101w0yl.5dx\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
